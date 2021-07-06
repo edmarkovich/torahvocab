78f0ff2f58ce0bcdf45165ade5a8b5945d6f9b35
Ed Markovich <em1258@stern.nyu.edu>
2021-07-05 21:16:43 -0400

    built book selector

--- frontend/src/VerseSelector.js
+++ frontend/src/VerseSelector.js
@@ -4,6 +4,9 @@ import React, { useState, useEffect } from 'react';
 function VerseSelector(props) {
 
     const [shape, setShape] = useState([]);
+    const [book, setBook]   = useState("Genesis")
+ 
+    const books = ["Genesis"  ,  "Exodus"  ,  "Leviticus"  ,  "Numbers"  ,  "Deuteronomy"]
 
     function next() { 
         props.setVerse(props.verse+1) 
@@ -14,18 +17,37 @@ function VerseSelector(props) {
     }
     function prev() { props.setVerse(props.verse-1) }
 
+    function nextBook() {
+      var idx = books.indexOf(book)
+      if (idx < books.length-1) {
+        setBook(books[idx+1])
+      }
+    }
+
+    function prevBook() {
+      var idx = books.indexOf(book)
+      if (idx > 0) {
+        setBook(books[idx-1])
+      }
+    }  
 
     useEffect( () => {
-        return fetch('http://localhost:5000/shape?book=Leviticus')
+        return fetch('http://localhost:5000/shape?book='+book)
         .then(data => data.json())
         .then(x => setShape(x))
-    }, [])
+    }, [book])
 
     return (
-        <div  className="controls" >
+        <div className="controls" >
+          <span onClick={nextBook}>←</span>
+          {book} 
+          <span onClick={prevBook}>→</span>
+
+        <br/>
+
+        {props.chapter+1}:{props.verse+1} / {shape[props.chapter]}
         <span onClick={next}>Next</span>&nbsp;📜&nbsp;
         <span onClick={prev}>Previous</span><br/>
-        Genesis {props.chapter+1}:{props.verse+1} / {shape[props.chapter]}
         </div>    
     )
 }
